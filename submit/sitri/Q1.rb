b66efa29308823a119e9ab971c68c400f8a2cea6
sitri <wc348nka@gmail.com>
2014-07-12 16:48:07 +0900

    FizzBuzz追加。（やるのわすれてた）

--- submit/sitri/Q1.rb
+++ submit/sitri/Q1.rb
@@ -20,11 +20,41 @@ for i in 1..9
     print "\n"
 end
 
-p ""
+print "\n"
+
 # each
 (1..9).each do  |a|
     (1..9).each do |b|
         printf("%02d ", a*b)
     end
     print "\n"
+end
+
+print "\n"
+
+
+# FizzBuzz
+for i in 1..100
+    is_FizzBuzz = false
+    
+    # ３で割り切れたら、Fizzを表示
+    if i % 3 == 0
+        print "Fizz"
+        is_FizzBuzz = true
+    end
+    
+    # ５で割り切れたら、Buzzを表示
+    if i % 5 == 0
+        print "Buzz"
+        is_FizzBuzz = true
+    end
+    
+    # FizzBuzzじゃなければ、数値を表示
+    unless is_FizzBuzz
+        print i
+    end
+    
+    # 改行
+    print "\n"
+    
 end

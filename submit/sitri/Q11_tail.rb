de5fe4f21659327cc1f79cd214cdd05665fb0392
sitri <wc348nka@gmail.com>
2014-10-11 19:46:44 +0900

    tailおわった

--- submit/sitri/Q11_tail.rb
+++ submit/sitri/Q11_tail.rb
@@ -1,11 +1,20 @@
-def tail(file_name)
+def tail(read_lineno, file_name)
   open(file_name){ |io|
-    #とりあえず全部読み込んで、行数だけ取得
-    io.each_line{} #　最終行に
-    p io.lineno
+  
+    file_line_count = io.count
     
-    io.seek(0, IO::SEEK_SET) # ファイルポインタをファイルの先頭に移動
+    io.seek(0, IO::SEEK_SET) # io.countでファイルポインタがEOFにあるので、ファイルの先頭に戻す。
+    io.lineno = 0 # io.countでファイルの行数がはいっちゃってるので、初期化。
     
+    # もし、ファイルの行数＜読み込む行数　ならば、読み込む行数＝ファイルの行数。
+    read_lineno = file_line_count < read_lineno ? file_line_count : read_lineno
+    
+    io.each_line{ |line|
+      if io.lineno >= file_line_count - read_lineno + 1 then
+        p line
+      end
+    }
   }
 end
-$*.each { |file_name| tail(file_name) }
+
+tail($*[0].to_i, $*[1])

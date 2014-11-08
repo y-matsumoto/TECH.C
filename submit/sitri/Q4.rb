636e696ac3b9a5c72fb25eabb933b21de35f7701
sitri@ndxbn <wc348nka@gmail.com>
2014-11-08 19:21:07 +0900

    途中。なんとなくクラス切れたけど微妙。

--- submit/sitri/Q4.rb
+++ submit/sitri/Q4.rb
@@ -3,11 +3,14 @@ class Deck
   attr_accessor :deck, :length #外からRW両方できる
   attr_writer :willShuffle #外からはWのみ
   
+  # 初期化
+  # @param length デッキの枚数
   def initialize(length)
     @length = length
     @deck = Array.new(@length){ |key| key+1 }
   end
   
+  # デッキシャッフル
   def shuffle()
     @deck.shuffle!
   end
@@ -16,6 +19,8 @@ end
 
 class Hand
   attr_accessor :length
+  # 初期化
+  # @param 手札の枚数
   def initialize(length=0)
     @length = length
   end
@@ -23,31 +28,61 @@ end
 
 class Game
   @deck
-  attr_accessor :hand
+  @hand
+  @ansList
   
+  # 初期化
   def initialize()
     @deck = Deck.new(9).shuffle
     @hand = @deck.shift(3)
+    @ansList = Array.new()
   end
   
-  def answer(ans)
-    unless 3 == ans.length then
-      p "your answer's length is required 3."
-      p "retry input"
+  # 答える
+  def hitAndBlow(ans)
+    unless validityAnswer?(ans) # 入力値チェック
       return
     end
+    
+    # 入力が大丈夫ならば、答え一覧にプッシュ
+    @ansList.push( ans.split("") )
+    p @ansList
   end
   
-  def collectAnswer?
+  # ansの文字列の妥当性
+  def validityAnswer?(ans)
+    # 3ケタの数字？
+    unless /^\d{3}$/ =~ ans
+      p "NOT 3 digit nums"
+      return false
+    end
+    
+    # 重複なし？
+    ans_a = ans.split("")
+    unless ans_a == ans_a.uniq
+      p "NOT Unique"
+      return false
+    end
+    
+    # エラーがなければ
     return true
-    #return false
   end
   
+  # 正解か？
+  def collectAnswer?
+    # もし、最後の回答が正解ならば、Trueで戻る。
+    if @ansList.last(1) == @hand
+      return true
+    end
+    # デフォルトは、Falseで戻る。
+    return false
+  end
   
 end
 
 #3つの異なる数字を選ぶ
 game = Game.new
 begin
-
+  game.hitAndBlow(gets.strip)
+  
 end until game.collectAnswer?

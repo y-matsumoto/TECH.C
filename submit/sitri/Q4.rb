299d7d3fc9b2c1e0c435b4f73dfc6060d8735c55
sitri@ndxbn <wc348nka@gmail.com>
2014-11-08 17:24:12 +0900

    no message

--- submit/sitri/Q4.rb
+++ submit/sitri/Q4.rb
@@ -1,20 +1,15 @@
-class Game
-
-end
-
 class Deck 
   #attr :tmp # 外からRのみ。attr_readerと同じ。
-  attr_accessor :deck, :deckLength #外からRW両方できる
+  attr_accessor :deck, :length #外からRW両方できる
   attr_writer :willShuffle #外からはWのみ
   
-  def initialize(length, willShuffle=false)
-    @length = deckLength
+  def initialize(length)
+    @length = length
     @deck = Array.new(@length){ |key| key+1 }
-    @willShuffle = willShuffle
-    if true==@willShuffle then
-      @deck.shuffle!
-    end
-    
+  end
+  
+  def shuffle()
+    @deck.shuffle!
   end
   
 end
@@ -26,8 +21,33 @@ class Hand
   end
 end
 
+class Game
+  @deck
+  attr_accessor :hand
+  
+  def initialize()
+    @deck = Deck.new(9).shuffle
+    @hand = @deck.shift(3)
+  end
+  
+  def answer(ans)
+    unless 3 == ans.length then
+      p "your answer's length is required 3."
+      p "retry input"
+      return
+    end
+  end
+  
+  def collectAnswer?
+    return true
+    #return false
+  end
+  
+  
+end
 
 #3つの異なる数字を選ぶ
-tmpDeck = Deck.new(10, true)
-hand = tmpDeck.deck.shift(3)
+game = Game.new
+begin
 
+end until game.collectAnswer?

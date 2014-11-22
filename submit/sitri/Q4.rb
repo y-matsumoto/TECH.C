097ac7f64afe603bd4e6bddb485d94cb6463d2ff
sitri <wc348nka@gmail.com>
2014-11-22 18:42:02 +0900

    Q4 hit and blow finished

--- submit/sitri/Q4.rb
+++ submit/sitri/Q4.rb
@@ -1,7 +1,6 @@
 class Deck 
   #attr :tmp # 外からRのみ。attr_readerと同じ。
   attr_accessor :deck, :length #外からRW両方できる
-  attr_writer :willShuffle #外からはWのみ
   
   # 初期化
   # @param length デッキの枚数
@@ -30,23 +29,51 @@ class Game
   @deck
   @hand
   @ansList
+  attr_reader :fin
   
   # 初期化
   def initialize()
     @deck = Deck.new(9).shuffle
     @hand = @deck.shift(3)
     @ansList = Array.new()
+    @fin = false
   end
   
   # 答える
-  def hitAndBlow(ans)
+  def answer(ans)
+    if ans == "0" then
+      @fin = true
+      return 
+    end
     unless validityAnswer?(ans) # 入力値チェック
       return
     end
     
-    # 入力が大丈夫ならば、答え一覧にプッシュ
-    @ansList.push( ans.split("") )
-    p @ansList
+    # (入力が大丈夫ならば、)
+    # ansを配列にして
+    ans = ans.split("").collect{|c| c.to_i}
+    #答え一覧にプッシュ
+    @ansList.push( ans )
+    #p ans #確認用の表示だけど、残しておいても良き
+    
+    hit = 0;
+    blow = 0;
+    no = 0;13
+    ans.each_index { |i|
+      case @hand.index( ans[i] )
+      when i
+        hit += 1
+      when nil
+        no += 1
+      else
+        blow += 1
+      end
+    }
+    
+    p "[#{@ansList.length}] hit: #{hit}, blow: #{blow}"
+    if hit == 3 then 
+      @fin = true
+    end
   end
   
   # ansの文字列の妥当性
@@ -67,22 +94,10 @@ class Game
     # エラーがなければ
     return true
   end
-  
-  # 正解か？
-  def collectAnswer?
-    # もし、最後の回答が正解ならば、Trueで戻る。
-    if @ansList.last(1) == @hand
-      return true
-    end
-    # デフォルトは、Falseで戻る。
-    return false
-  end
-  
 end
 
 #3つの異なる数字を選ぶ
 game = Game.new
 begin
-  game.hitAndBlow(gets.strip)
-  
-end until game.collectAnswer?
+  game.answer(gets.strip)
+end until game.fin
